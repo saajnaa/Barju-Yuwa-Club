3970b76d251dd53ceb7b95b7259973fbd116c580
Sajan Rishidev <saajnaa.123@gmail.com>
2025-10-19 13:11:26 +0545

    og image update

--- frontend/src/component/og/Og.jsx
+++ frontend/src/component/og/Og.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
+import heroimg from '../../assets/img/events/cloths-bitran/cb3.jpg'
+
 const Og = () => (
   <Helmet>
     <title>Barju Yuwa Club</title>
     <meta property="og:title" content="Barju Yuwa Club" />
     <meta property="og:description" content="Barju Yuwa Club - Youth Empowerment and Community Service." />
-    <meta property="og:image" content="https://example.com/image.jpg" />
+    <meta property="og:image" content={heroimg} />
     <meta property="og:url" content="https://example.com" />
     <meta property="og:type" content="website" />
   </Helmet>
