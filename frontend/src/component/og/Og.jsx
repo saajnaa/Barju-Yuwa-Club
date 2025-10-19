259e8503c932b82f230fcc7c1a54462427391be1
Sajan Rishidev <saajnaa.123@gmail.com>
2025-10-19 13:19:34 +0545

    vercel link updated

--- frontend/src/component/og/Og.jsx
+++ frontend/src/component/og/Og.jsx
@@ -8,7 +8,7 @@ const Og = () => (
     <title>Barju Yuwa Club</title>
     <meta property="og:title" content="Barju Yuwa Club" />
     <meta property="og:description" content="Barju Yuwa Club - Youth Empowerment and Community Service." />
-    <meta property="og:image" content="https://your-vercel-domain.vercel.app/assets/img/events/cloths-bitran/cb3.jpg" />
+    <meta property="og:image" content="https://barju-yuwa-club.vercel.app/assets/img/events/cloths-bitran/cb3.jpg" />
     <meta property="og:url" content="https://example.com" />
     <meta property="og:type" content="website" />
   </Helmet>
