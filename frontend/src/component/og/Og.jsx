96f4aaf0420d5db5d08bce5b1f20a00d0951324c
Sajan Rishidev <saajnaa.123@gmail.com>
2025-10-19 13:04:03 +0545

    og component error fixed

--- frontend/src/component/og/Og.jsx
+++ frontend/src/component/og/Og.jsx
@@ -2,17 +2,14 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 const Og = () => (
-  <div>
-    <Helmet>
-      <title>Barju Yuwa Club</title>
-      <meta property="og:title" content="My Awesome Page" />
-      <meta property="og:description" content="This is an awesome page description." />
-      <meta property="og:image" content="https://example.com/image.jpg" />
-      <meta property="og:url" content="https://example.com" />
-      <meta property="og:type" content="website" />
-    </Helmet>
-    <h1>Welcome to My Awesome Page</h1>
-  </div>
+  <Helmet>
+    <title>Barju Yuwa Club</title>
+    <meta property="og:title" content="Barju Yuwa Club" />
+    <meta property="og:description" content="Barju Yuwa Club - Youth Empowerment and Community Service." />
+    <meta property="og:image" content="https://example.com/image.jpg" />
+    <meta property="og:url" content="https://example.com" />
+    <meta property="og:type" content="website" />
+  </Helmet>
 );
 
 export default Og;
